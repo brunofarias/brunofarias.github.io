d2a8219da7f2590a771180290e8277b9662202f1
Bruno <begfarias@gmail.com>
2020-07-07 21:41:38 -0300

    Adicionando timezone

--- dist/timezone.php
+++ dist/timezone.php
@@ -1,5 +1,5 @@
 <?php
- date_default_timezone_set('America/Recife');
+ date_default_timezone_set('US/Eastern');
  $currenttime = date("m-d-Y H:i:s");
  list($ddd,$ttt) = split(' ',$currenttime);
  echo "$ddd/$ttt\n";
